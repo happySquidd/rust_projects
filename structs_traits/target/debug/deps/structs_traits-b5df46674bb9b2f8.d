4c87f921b8d31b9104ccd9f10bbdf9ac60cd93f5
happySquidd <rafael.safin077@gmail.com>
2025-10-12 23:53:52 -0400

    structs and traits challenge complete

---
+++ structs_traits/target/debug/deps/structs_traits-b5df46674bb9b2f8.d
@@ -0,0 +1,5 @@
+/home/Jonny/Documents/mycode/rust_practice/rust_projects/structs_traits/target/debug/deps/structs_traits-b5df46674bb9b2f8.d: src/main.rs
+
+/home/Jonny/Documents/mycode/rust_practice/rust_projects/structs_traits/target/debug/deps/structs_traits-b5df46674bb9b2f8: src/main.rs
+
+src/main.rs:
---
+++ structs_traits/target/debug/structs_traits.d
@@ -0,0 +1,1 @@
+/home/Jonny/Documents/mycode/rust_practice/rust_projects/structs_traits/target/debug/structs_traits: /home/Jonny/Documents/mycode/rust_practice/rust_projects/structs_traits/src/main.rs
